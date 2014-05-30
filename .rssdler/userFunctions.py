a8a9be1e0ddad0370ec54182c27ad57425871021
tatou-tatou <tatou-tatou@users.noreply.github.com>
2014-05-30 22:05:07 +0200

    Minor changes

---
+++ .rssdler/userFunctions.py
@@ -0,0 +1,8 @@
+def sendNotification(directory, filename, rssItemNode, retrievedLink, downloadDict, threadName):
+    import os
+    if not 'DISPLAY' in os.environ:
+        os.environ['DISPLAY'] = ':0'
+    from gi.repository import Notify
+    Notify.init ("New torrent")
+    Message=Notify.Notification.new ("New torrent", filename,"/home/tatou/.icons/Tatou/apps/48x48/rss.png")
+    Message.show ()
